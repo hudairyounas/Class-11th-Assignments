716065de22f7fe60dfe63b950ddd22d0f6e48c4d
Hudair Younis <hudairshorts@gmail.com>
2025-09-13 14:41:24 +0500

    assignment 1

---
+++ assignment 1/python.py
@@ -0,0 +1,12 @@
+# Write a program that print Hello World
+print("Hello World")
+
+# Write a program that print your name, roll no and matric marks
+
+name = "Hudair"
+rollNo = 471871
+number = 566
+
+print("Name", name)
+print("Roll No", rollNo)
+print("Matric Number", number)
